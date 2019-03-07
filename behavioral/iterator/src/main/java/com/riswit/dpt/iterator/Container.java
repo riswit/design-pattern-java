6266ac29d8a6942e00a3123a39722fe7d7632211
Dino <dino.solano@riswit.it>
2019-03-07 21:32:16 +0100

    Iterator design pattern

---
+++ behavioral/iterator/src/main/java/com/riswit/dpt/iterator/Container.java
@@ -0,0 +1,5 @@
+package com.riswit.dpt.iterator;
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ behavioral/iterator/src/main/java/com/riswit/dpt/iterator/Iterator.java
@@ -0,0 +1,6 @@
+package com.riswit.dpt.iterator;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
