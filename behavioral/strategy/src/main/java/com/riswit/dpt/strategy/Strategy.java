1849e255d0525f3b4fcb680614b5aabca6dac104
Dino <dino.solano@riswit.it>
2019-03-14 20:26:57 +0100

    Strategy design pattern

---
+++ behavioral/strategy/src/main/java/com/riswit/dpt/strategy/Strategy.java
@@ -0,0 +1,5 @@
+package com.riswit.dpt.strategy;
+
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
---
+++ behavioral/strategy/src/main/java/com/riswit/dpt/strategy/info_strategy.md
@@ -0,0 +1,2 @@
+
+https://www.tutorialspoint.com/design_pattern/strategy_pattern.htm
